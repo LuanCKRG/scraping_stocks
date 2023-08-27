6e82ac7ed769b57e1d852fd806dcd0a38522cbbe
LuanCKRG <crackersage@gmail.com>
2023-08-27 15:50:36 -0400

    feat: add SearchBar component to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Header } from "@/components/Header"
+import { SearchBar } from "@/components/SearchBar"
 
 const Home = () => {
   return (
     <>
       <Header />
+      
+      <div className="flex flex-col">
+				<SearchBar />
+			</div>
     </>
   )
 }
