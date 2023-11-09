2ea672aa5b47bbcd6ca50792083dd5385073ae53
LuanCKRG <crackersage@gmail.com>
2023-11-09 01:54:15 -0400

    fix(4): 09/11/23

--- src/app/dashboard/stocks/[ticker]/page.tsx
+++ src/app/dashboard/stocks/[ticker]/page.tsx
@@ -4,6 +4,8 @@ import { Database } from "@/types"
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 
+export const dynamic = "force-dynamic"
+
 const Page = async ({params}: {params: {ticker: string}}) => {
   const ticker = params.ticker.toUpperCase()
 
