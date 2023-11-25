dce29c3cf4be2b86ca3513d467015b6c76ad612a
LuanCKRG <crackersage@gmail.com>
2023-11-25 18:25:31 -0400

    25/11/23 2

--- src/app/dashboard/fundos/page.tsx
+++ src/app/dashboard/fundos/page.tsx
@@ -5,12 +5,16 @@ import { cookies } from "next/headers"
 const Page = () => {
   const supabase = createServerComponentClient<Database>({ cookies })
 
-  const { data: {publicUrl} } = supabase.storage.from("funds").getPublicUrl("public/funds_explorer.xlsx")
+  const { data: {publicUrl: fundsUrl} } = supabase.storage.from("funds").getPublicUrl("public/funds_explorer.xlsx")
+  const { data: {publicUrl: fundamentusUrl} } = supabase.storage.from("funds").getPublicUrl("public/fundamentus.xlsx")
  
   return (
-    <div>
-      <a className="underline" href={publicUrl}>
+    <div className="flex flex-col">
+      <a className="underline" href={fundsUrl}>
         Download planilha fundsexplorer
+      </a>
+      <a className="underline mt-2" href={fundamentusUrl}>
+        Download planilha fundamentus
       </a>
     </div>
   )
