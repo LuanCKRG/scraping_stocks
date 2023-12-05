29360ab5a00e794bb93c44ddb458cd59d7842cc7
LuanCKRG <crackersage@gmail.com>
2023-12-04 23:54:40 -0400

    04/12/23 2

--- src/app/dashboard/sheets/page.tsx
+++ src/app/dashboard/sheets/page.tsx
@@ -1,6 +1,9 @@
 import { Database } from "@/types"
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
+import Image from "next/image"
+import bcb1 from "@/assets/bcb1.png"
+import bcb2 from "@/assets/bcb2.png"
 
 const Page = async () => {
   const supabase = createServerComponentClient<Database>({ cookies })
@@ -10,60 +13,64 @@ const Page = async () => {
 
 
   return (
-    <div className="flex flex-col m-4 items-start space-y-4 justify-center sm:space-y-0 sm:grid sm:gap-4 sm:grid-cols-2 lg:gap-8 lg:grid-cols-3 px-4">
-      {
-        exchanges?.map(
-          (exchange, index) => {
-            return (
-              <section className="flex flex-col border-2 rounded-lg p-2">
-                <div className="w-full border-b-2 border-black">
-                  <h2 className="text-center">
-                    <b> {exchange.title} </b>
-                  </h2>
-                </div>
-                <div>
+    <>
+      <Image className="w-full" src={bcb1} alt={"Banco central dados"} />
+      <Image className="w-full" src={bcb2} alt={"Banco central dados"} />
+      <div className="flex flex-col m-4 items-start space-y-4 justify-center sm:space-y-0 sm:grid sm:gap-4 sm:grid-cols-2 lg:gap-8 lg:grid-cols-3 px-4">
+        {
+          exchanges?.map(
+            (exchange, index) => {
+              return (
+                <section className="flex flex-col border-2 rounded-lg p-2">
+                  <div className="w-full border-b-2 border-black">
+                    <h2 className="text-center">
+                      <b> {exchange.title} </b>
+                    </h2>
+                  </div>
+                  <div>
                     <div className="space-y-0 grid gap-4 grid-cols-4 px-4 border-b-2 border-black">
                       {exchange.table.map(
                         (table, index) => {
                           return (
 
-                        <div className="flex flex-col items-center justify-center">
-                          <b>
-                             {
-                              // @ts-expect-error
-                                table?.year
-                             }
-                          </b>
-                          <p>
-                          {
-                              // @ts-expect-error
-                                table?.cambio
-                             }
-                          </p>
-                        </div>
+                            <div className="flex flex-col items-center justify-center">
+                              <b>
+                                {
+                                  // @ts-expect-error
+                                  table?.year
+                                }
+                              </b>
+                              <p>
+                                {
+                                  // @ts-expect-error
+                                  table?.cambio
+                                }
+                              </p>
+                            </div>
                           )
                         }
                       )}
                     </div>
-                  <div>
-                    Data: <b> {exchange.date} </b>
+                    <div>
+                      Data: <b> {exchange.date} </b>
+                    </div>
+                    <div>
+                      Orgão: <b> {exchange.src} </b>
+                    </div>
                   </div>
-                  <div>
-                    Orgão: <b> {exchange.src} </b>
+                  <div className="mx-auto underline">
+                    <a href={exchange.href!} >
+                      Fonte
+                    </a>
                   </div>
-                </div>
-                <div className="mx-auto underline">
-                  <a href={exchange.href!} >
-                    Fonte
-                  </a>
-                </div>
-              </section>
-            )
-          }
-        )
-      }
-      {/* <pre>{JSON.stringify(exchanges, null, 2)}</pre> */}
-    </div>
+                </section>
+              )
+            }
+          )
+        }
+        {/* <pre>{JSON.stringify(exchanges, null, 2)}</pre> */}
+      </div>
+    </>
   )
 }
 
